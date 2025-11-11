8fc25a1969eec961e0af493518aa87f482b8334e
jnakaso <jnakaso@yahoo.com>
2025-11-11 13:56:33 -0800

    Example code for auto-save

--- src/pages/grants/GrantRecipesDetailPage.tsx
+++ src/pages/grants/GrantRecipesDetailPage.tsx
@@ -1,10 +1,46 @@
-import { Typography } from "@mui/material";
+/**
+ *  GrantRecipesDetailPage.tsx
+ *
+ *  @copyright 2025 Digital Aid Seattle
+ *
+ */
+import { Button, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { grantRecipeService } from "../../services/grantRecipeService";
+import { GrantRecipe } from "../../types";
 
 const GrantRecipesDetailPage: React.FC = () => {
+  const [recipe, setRecipe] = useState<GrantRecipe>();
+  const [dirty, setDirty] = useState<boolean>(false);
+
+
+  useEffect(() => {
+    if (dirty) {
+      // debounce a bit
+      const id = setInterval(doSave, 2000);
+      return () => clearInterval(id);
+    }
+  }, [dirty]);
+
+  const handleOutputFieldChange = (index: number, field: 'name' | 'maxWords', value: string | number) => {
+    setDirty(true);
+  };
+
+  function doSave() {
+    if (recipe) {
+      grantRecipeService.update(recipe.id!, recipe)
+        .then(updated => {
+          setRecipe(updated);  // May have to worry about state here, inputs may update unexpectantly
+          setDirty(false);
+        })
+    }
+  }
+
   return (
     <div>
       <Typography variant="h4">Grant Recipe Detail</Typography>
       <Typography>This is a blank detail page for grant recipes.</Typography>
+      <Button onClick={() => handleOutputFieldChange(1, 'name', 'newName')}>Test</Button>
     </div>
   );
 };
