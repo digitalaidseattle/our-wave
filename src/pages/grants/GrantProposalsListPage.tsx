232c7f8a06bfed86223f5b1bc06750c55ba3ad02
shardareddy <shardapreddy@gmail.com>
2026-07-27 11:35:31 -0700

    Refactor - grantProposalService to be a singleton class instead of a module with functions. This allows for better testability and easier mocking of the service in tests.

--- src/pages/grants/GrantProposalsListPage.tsx
+++ src/pages/grants/GrantProposalsListPage.tsx
@@ -24,6 +24,7 @@ import { PROPOSAL_LABELS } from "../../constants/labels";
 const LABELS = PROPOSAL_LABELS;
 
 const GrantProposalsListPage: React.FC = () => {
+  const grantProposalService = GrantProposalService.getInstance();
   const notifications = useNotifications();
 
   const navigate = useNavigate();
